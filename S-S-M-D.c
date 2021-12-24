3c766cd482f3c2f03ba1baf75d276d774c9f2259
KrutarthKatrodiya <96043997+KrutarthKatrodiya@users.noreply.github.com>
2021-12-24 00:00:24 +0000

    Create S-S-M-D.c

---
+++ S-S-M-D.c
@@ -0,0 +1,18 @@
+#include<Stdio.h>
+
+void main()
+{
+    Int a, b, c;
+    printf("Enter Value of a: ");
+    scanf("%d", &a);
+    printf("Enter Value of b: ");
+    scanf("%d", &b);
+    c=a+b;
+    c=a-b;
+    c=a*b;
+    c=a/b;
+    printf("Addition is %d,c");
+    printf("Subtraction is %d,c");
+    printf("Multiplication is %d,c");
+    printf("Division is %d,c");
+}
